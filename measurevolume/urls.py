d30b23ec44880fc3fc29566fe88f2cd1c6019599
Morisaki Mikiya <git@morisakimikiya.com>
2021-06-27 17:36:31 +0900

    Add calc_volume function

--- measurevolume/urls.py
+++ measurevolume/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("calc_volume/", views.calc_volume, name="calc_volume"),
 ]
--- measurevolume/views.py
+++ measurevolume/views.py
@@ -1,5 +1,33 @@
 from django.shortcuts import render
+from django.views.decorators.http import require_http_methods
+from django.http.request import HttpRequest
+from django.http.response import JsonResponse
+
+import cv2
 
 
 def index(request):
     return render(request, "measurevolume/index.html")
+
+
+@require_http_methods(["POST"])
+def calc_volume(request: HttpRequest) -> JsonResponse:
+    """水の容量を計算する
+
+    Args:
+        request (HttpRequest): Django HttpRequest オブジェクト
+                               request.POST["img_base64"]でbase64エンコード画像取得
+
+    Returns:
+        JsonResponse: JsonResponse オブジェクトは Django HttpResponse クラスのサブクラス
+                                   以下を返す
+                                   {
+                                       exist_glass (boolean): コップの有無,
+                                       exist_chopsticks (boolean): 割り箸の有無,
+                                       volume (number): 容量(mL),
+                                   }
+
+    Note:
+        POSTメソッドのみ受け付ける
+    """
+    pass
