49422634bf2bb0635b0a2f60d4c73a9cbefa4c43
Morisaki Mikiya <git@morisakimikiya.com>
2021-06-27 16:03:41 +0900

    Add front sample files

--- measurevolume/views.py
+++ measurevolume/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
 def index(request):
-    return HttpResponse("Hello World")
+    return render(request, "measurevolume/index.html")
