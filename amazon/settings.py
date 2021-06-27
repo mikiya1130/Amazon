ed4bc674e119dfbfce71bc8cafd925a70f5456e3
Morisaki Mikiya <git@morisakimikiya.com>
2021-06-27 15:57:46 +0900

    Update top path

--- amazon/settings.py
+++ amazon/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    "measurevolume.apps.MeasurevolumeConfig",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
