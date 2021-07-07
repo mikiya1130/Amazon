12f013e24a6cb4b012cf97a926a4c06f4e3afaf9
Morisaki Mikiya <git@morisakimikiya.com>
2021-07-07 15:41:31 +0900

    Update test func

--- measurevolume/tests.py
+++ measurevolume/tests.py
@@ -1,11 +1,78 @@
 from django.test import TestCase
-from django.test import Client
+from django.urls import reverse
+
+import numpy as np
+import cv2
+import base64
+import json
+from unittest import mock
+
+
+def create_image(height=480, width=640, color=(0, 0, 0)):
+    """単色のダミー画像を生成する
+
+    Args:
+        height (int, optional): 高さ. Defaults to 480.
+        width (int, optional): 幅. Defaults to 640.
+        color (tuple, optional): RGB. Defaults to (0, 0, 0).
+
+    Returns:
+        (np.ndarray): 画像データ.
+    """
+    img = np.zeros((height, width, 3))
+    img += color[::-1]
+    return img
 
 
 class CalcVolumeViewTests(TestCase):
-    def test_glass_and_chopsticks(self):
+    @mock.patch("measurevolume.views.detect_water_area")
+    @mock.patch("measurevolume.views.get_chopsticks_length_per_pixel")
+    def test_base64_to_img(
+        self, mock_get_chopsticks_length_per_pixel, mock_detect_water_area
+    ):
+        """base64から画像データへの変換をテスト"""
+        mock_detect_water_area.return_value = create_image()  # dummy
+        mock_get_chopsticks_length_per_pixel.return_value = 0.1  # dummy
+
+        img = create_image()
+        _, buf = cv2.imencode(".png", img)
+        img_base64 = base64.b64encode(buf).decode("utf-8")
+
+        data = {"img_base64": img_base64}
+
+        _ = self.client.post(
+            reverse("calc_volume"),
+            data=json.dumps(data),
+            content_type="application/json",
+        )
+
+        self.assertTrue(np.array_equal(img, mock_detect_water_area.call_args[0][0]))
+
+    @mock.patch("measurevolume.views.detect_water_area")
+    @mock.patch("measurevolume.views.get_chopsticks_length_per_pixel")
+    def test_glass_and_chopsticks(
+        self, mock_get_chopsticks_length_per_pixel, mock_detect_water_area
+    ):
         """コップと割り箸の検出をテスト"""
-        pass
+        mock_detect_water_area.return_value = create_image()
+        mock_get_chopsticks_length_per_pixel.return_value = 0.1
+
+        img = create_image()
+        _, buf = cv2.imencode(".png", img)
+        img_base64 = base64.b64encode(buf).decode("utf-8")
+
+        data = {"img_base64": img_base64}
+
+        response = self.client.post(
+            reverse("calc_volume"),
+            data=json.dumps(data),
+            content_type="application/json",
+        )
+
+        content = json.loads(response.content)
+
+        self.assertTrue(content["exist_glass"])
+        self.assertTrue(content["exist_chopsticks"])
 
     def test_glass(self):
         """コップの検出をテスト"""
